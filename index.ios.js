713b5d401b97e22373ae3b4fb0e4e44d68176a71
Fede Defaveri <f.g.defaveri@gmail.com>
2017-01-19 17:32:32 -0500

    added an initial route pointing to Main component, also added styles to build the initial UI

--- index.ios.js
+++ index.ios.js
@@ -9,44 +9,36 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  NavigatorIOS,
+  View,
 } from 'react-native';
+var Main = require('./App/Components/Main')
 
 export default class githubNotetaker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '', //the name we input
+      isLoading: false,
+      error: false
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Mobile development!!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS 
+        style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }} />
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111111'
   },
 });
 
