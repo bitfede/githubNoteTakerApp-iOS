7a09e59360a833534d36fa2d32225c2af873e749
Fede Defaveri <f.g.defaveri@gmail.com>
2017-01-23 22:43:27 -0500

    added image in dashboard component, added 3 buttons to explore profile, repos, notes

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -2,18 +2,68 @@ import React, { Component } from 'react'
 import {
 	Text,
 	View,
-	StyleSheet
+	StyleSheet,
+  Image,
+  TouchableHighlight 
 } from 'react-native'
 
 class Dashboard extends Component {
+  makeBackground(btn) {
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+
+    if (btn === 0) {
+      obj.backgroundColor = '#48BBEC'
+    }
+    else if (btn === 1) {
+      obj.backgroundColor = '#E77AAE'
+    }
+    else {
+      obj.backgroundColor = '#758BF4'
+    }
+
+    return obj
+  }
+
+  goToProfile() {
+    console.log("going to profile")
+  }
+  goToRepos() {
+    console.log("Going to repos")
+  }
+  goToNotes() {
+    console.log("going to notes")
+  }
+
 	render() {
 		return (
 				<View style={styles.container}>
-					<Text>This is the dashboard</Text>
-					<Text>user {this.props.userInfo.toString()} </Text>
+          <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image}/>
+					<TouchableHighlight
+            style={this.makeBackground(0)}
+            onPress={this.goToProfile.bind(this)}
+            underlayColor='#88D4F5'>
+              <Text style={styles.buttonText}> View Profile </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={this.makeBackground(1)}
+            onPress={this.goToRepos.bind(this)}
+            underlayColor='#88D4F5'>
+              <Text style={styles.buttonText}> View Repos </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={this.makeBackground(2)}
+            onPress={this.goToNotes.bind(this)}
+            underlayColor='#88D4F5'>
+              <Text style={styles.buttonText}> View Notes </Text>
+          </TouchableHighlight>
 				</View>
 		)
-	}
+	} 
 }
 
 var styles = StyleSheet.create({
